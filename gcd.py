58cecba6826cd89440884d90d3f6cdd18d80f923
Samuel <samuelorlato@gmail.com>
2022-07-06 16:38:22 -0300

    GCD with 2 numbers

--- gcd.py
+++ gcd.py
@@ -1,7 +1,14 @@
 from Calc import Calc
+from lcm import LCM
 
 class GCD(Calc):
     def calc(self, *args):
         for i in args[0]: args[0][args[0].index(i)] = int(args[0][args[0].index(i)])
 
-        return args[0]
+        lcm = LCM()
+
+        p = 1
+        for i in args[0]:
+            p *= i
+        
+        return p / lcm.calc(args[0])
